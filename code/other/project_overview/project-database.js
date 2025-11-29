dbf40addf43260bc67191494da4c86134b84a54a
warezoid <warezoid@proton.me>
2025-11-29 19:36:26 +0100

    [ Version 1.12.5 ] - update projects function was written.

--- code/other/project_overview/project-database.js
+++ code/other/project_overview/project-database.js
@@ -5,7 +5,7 @@ export const allProjects = [
         link_params: `target="_blank"`,
         link_background: "./../../src/img/slideshow/bitcointags.png",
         type: "software",
-        date: 2025,
+        date: "2025",
         project_name: "Bitcointags",
         project_desc: "Browser extension that converts the fiat price of a product or service within a selected HTML entity into an approximate bitcoin price."
     },
@@ -15,7 +15,7 @@ export const allProjects = [
         link_params: "",
         link_background: "./../../src/img/slideshow/sponge_holder.png",
         type: "3d printing",
-        date: 2025,
+        date: "2025",
         project_name: "Sponge holder",
         project_desc: "Kitchen holder that can hold a sponge and dish detergent and liquid soap."
     },
@@ -25,7 +25,7 @@ export const allProjects = [
         link_params: "",
         link_background: "./../../src/img/overview/plant_marker.png",
         type: "3d printing",
-        date: 2024,
+        date: "2024",
         project_name: "Plant marker",
         project_desc: "Simple plant marker."
     },
@@ -35,7 +35,7 @@ export const allProjects = [
         link_params: "",
         link_background: "./../../src/img/overview/tv_antenna_mount.png",
         type: "3d printing",
-        date: 2024,
+        date: "2024",
         project_name: "TV antenna mount",
         project_desc: "Structure for mounting a TV antenna with zip ties, attached to the TV via VESA screw holes."
     }
--- code/other/project_overview/project-overview.js
+++ code/other/project_overview/project-overview.js
@@ -86,12 +86,54 @@ const projectOverview = document.getElementById("project-overview")
 let filterSettings = {
     type: {
         "software": true,
-        "3d_printing": true
+        "3d printing": true
     },
     date: {
         "2024": true,
         "2025": true
     }
+}
+
+const renderProjects = () => {
+    projectOverview.innerHTML = ""
+
+    allProjects.forEach((oneProject) => {
+        if(oneProject.active){
+            projectOverview.innerHTML += `
+                <a href="${oneProject.link_url}" ${oneProject.link_params}>
+                    <div class="one-project" style="background-image: url(${oneProject.link_background});">
+                        <div class="one-project-toolbar">
+                            <p>${oneProject.type}.</p>
+                            <p>${oneProject.date}</p>
+                        </div>
+
+                        <div class="one-project-text">
+                            <h3>${oneProject.project_name}</h3>
+                            <p>${oneProject.project_desc}</p>
+                        </div>
+                    </div>
+                </a>
+            `
+        }
+    })
+}
+renderProjects()
+
+const updateProjects = () => {
+    for(let i = 0; i < allProjects.length; i++){
+        let oneProject = allProjects[i]
+
+        if(filterSettings.type[oneProject.type]) {
+            if(filterSettings.date[oneProject.date]){
+                oneProject.active = true
+                continue
+            }
+        }
+
+        oneProject.active = false
+    }
+    
+    renderProjects()
 }
 
 
@@ -111,16 +153,18 @@ const checkbox2025 = document.getElementById("date-2025")
 
 const setCheckbox = () => {
     checkboxSoftware.checked = filterSettings.type["software"]
-    checkbox3dprinting.checked = filterSettings.type["3d_printing"]
+    checkbox3dprinting.checked = filterSettings.type["3d printing"]
     checkbox2024.checked = filterSettings.date["2024"]
     checkbox2025.checked = filterSettings.date["2025"]
 }
 
 const loadCheckbox = () => {
     filterSettings.type["software"] = checkboxSoftware.checked
-    filterSettings.type["3d_printing"] = checkbox3dprinting.checked
+    filterSettings.type["3d printing"] = checkbox3dprinting.checked
     filterSettings.date["2024"] = checkbox2024.checked
     filterSettings.date["2025"] = checkbox2025.checked
+
+    updateProjects()
 }
 
 filterBtn.addEventListener("click", () => {
@@ -129,7 +173,6 @@ filterBtn.addEventListener("click", () => {
     filterPopup.style.display = "flex"
     webpage.classList.add("blured")
 })
-
 filterCloseBtn.addEventListener("click", () => {
     loadCheckbox()
 
