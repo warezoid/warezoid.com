2acb1fc253160bfb89a5c9548a564860cc8ead8f
warezoid <warezoid@proton.me>
2026-01-08 12:44:23 +0100

    [ Version 1.17.2 ] - project overview filter was updated.

--- code/other/project_overview/project-overview.js
+++ code/other/project_overview/project-overview.js
@@ -86,11 +86,13 @@ const projectOverview = document.getElementById("project-overview")
 let filterSettings = {
     type: {
         "software": true,
-        "3d printing": true
+        "3d printing": true,
+        "IoT": true
     },
     date: {
         "2024": true,
-        "2025": true
+        "2025": true,
+        "2026": true
     }
 }
 
@@ -148,21 +150,27 @@ const filterCloseBtn = document.getElementById("close-button")
 
 const checkboxSoftware = document.getElementById("type-software")
 const checkbox3dprinting = document.getElementById("type-3dprinting")
+const checkboxIot = document.getElementById("type-iot")
 const checkbox2024 = document.getElementById("date-2024")
 const checkbox2025 = document.getElementById("date-2025")
+const checkbox2026 = document.getElementById("date-2026")
 
 const setCheckbox = () => {
     checkboxSoftware.checked = filterSettings.type["software"]
     checkbox3dprinting.checked = filterSettings.type["3d printing"]
+    checkboxIot.checked = filterSettings.type["IoT"]
     checkbox2024.checked = filterSettings.date["2024"]
     checkbox2025.checked = filterSettings.date["2025"]
+    checkbox2026.checked = filterSettings.date["2026"]
 }
 
 const loadCheckbox = () => {
     filterSettings.type["software"] = checkboxSoftware.checked
     filterSettings.type["3d printing"] = checkbox3dprinting.checked
+    filterSettings.type["IoT"] = checkboxIot.checked
     filterSettings.date["2024"] = checkbox2024.checked
     filterSettings.date["2025"] = checkbox2025.checked
+    filterSettings.date["2026"] = checkbox2026.checked
 
     updateProjects()
 }
