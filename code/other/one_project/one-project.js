efa02a463312c6919bc0234a782e7b45d2c2a260
warezoid <warezoid@proton.me>
2026-01-14 17:08:46 +0100

    [ Version 1.21 ] - photo gallery can be controled by arrow keys.

--- code/other/one_project/one-project.js
+++ code/other/one_project/one-project.js
@@ -98,53 +98,16 @@ const tween2 = KUTE.fromTo(
 
 
 // photo gallery
-const webpage = document.getElementById("webpage")
-const galleryPopup = document.getElementById("gallery-popup")
-const galleryCloseBtn = document.getElementById("gallery-popup-closeBtn")
-
-galleryCloseBtn.addEventListener("click", () => {
-    galleryPopup.style.display = "none"
-    webpage.classList.remove("blured")
-})
-
-
-
+const galleryPopupImage = document.getElementById("gallery-popup-image")
 const galleryContent = document.getElementById("gallery-content")
 const galleryContentImages = Array.from(galleryContent.children)
-const galleryPopupImage = document.getElementById("gallery-popup-image")
-
-galleryContent.addEventListener("click", (e) => {
-    if(e.target.nodeName == "IMG"){
-        galleryPopupImage.src = e.target.attributes.src.nodeValue
-        index = galleryContentImages.indexOf(e.target.parentElement)
-        galleryPopup.style.display = "flex"
-        webpage.classList.add("blured")
-    }
-})
-
-
-
-const backBtn = document.getElementById("gallery-popup-backButton")
-const forwardBtn = document.getElementById("gallery-popup-forwardButton")
 let index = -1
 
 const setImage = (i) => {
     galleryPopupImage.src = galleryContentImages[i].firstElementChild.attributes.src.nodeValue
 }
 
-backBtn.addEventListener("click", () => {
-    if(index != -1){
-        index--
-
-        if(index < 0){
-            index = galleryContentImages.length - 1
-        }
-
-        setImage(index)
-    }
-})
-
-forwardBtn.addEventListener("click", () => {
+const incrementIndex = () => {
     if(index != -1){
         index++
 
@@ -154,7 +117,68 @@ forwardBtn.addEventListener("click", () => {
 
         setImage(index)
     }
+}
+
+const decrementIndex = () => {
+    if(index != -1){
+        index--
+
+        if(index < 0){
+            index = galleryContentImages.length - 1
+        }
+
+        setImage(index)
+    }    
+}
+
+const handleKeyUp = (e) => {
+    switch(e.key){
+        case "ArrowLeft":
+            decrementIndex()
+            break
+        case "ArrowRight":
+            incrementIndex()
+            break;
+    }  
+}
+
+
+
+const webpage = document.getElementById("webpage")
+const galleryPopup = document.getElementById("gallery-popup")
+const galleryCloseBtn = document.getElementById("gallery-popup-closeBtn")
+
+galleryCloseBtn.addEventListener("click", () => {
+    galleryPopup.style.display = "none"
+    webpage.classList.remove("blured")
+    window.removeEventListener("keyup", handleKeyUp)
 })
+
+
+
+galleryContent.addEventListener("click", (e) => {
+    if(e.target.nodeName == "IMG"){
+        galleryPopupImage.src = e.target.attributes.src.nodeValue
+        index = galleryContentImages.indexOf(e.target.parentElement)
+        galleryPopup.style.display = "flex"
+        webpage.classList.add("blured")
+        window.addEventListener("keyup", handleKeyUp)
+    }
+})
+
+
+
+const backBtn = document.getElementById("gallery-popup-backButton")
+const forwardBtn = document.getElementById("gallery-popup-forwardButton")
+
+backBtn.addEventListener("click", () => {
+    decrementIndex()
+})
+
+forwardBtn.addEventListener("click", () => {
+    incrementIndex()
+})
+
 
 
 
